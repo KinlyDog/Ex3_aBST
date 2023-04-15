382798c781fa8cff478d59617f8c4e388eb81c77
KinlyDog <kinlydog@gmail.com>
2023-04-16 04:27:50 +1000

    Update project

--- src/aBST.java
+++ src/aBST.java
@@ -4,7 +4,13 @@ public class aBST {
     public Integer[] Tree;
 
     public aBST(int depth) {
-        int tree_size = (int) Math.pow(2, depth);
+        int tree_size = 0;
+
+        while (depth >= 0) {
+            tree_size += (int) Math.pow(2, depth);
+            depth--;
+        }
+
         Tree = new Integer[tree_size];
 
         for (int i = 0; i < tree_size; i++) {
@@ -13,18 +19,49 @@ public class aBST {
     }
 
     public Integer FindKeyIndex(int key) {
+        if (Tree != null) {
+            return FindKeyIndexRec(key, 0);
+        }
 
         return null;
     }
 
-    public int AddKey(int key) {
+    private Integer FindKeyIndexRec(int key, int i) {
+        if (i >= Tree.length) {
+            return null;
+        }
 
-        return -1;
+        if (Tree[i] == null) {
+            return -i;
+        }
+
+        if (Tree[i] == key) {
+            return i;
+        }
+
+        if (key < Tree[i]) {
+            return FindKeyIndexRec(key, 2 * i + 1);
+        } else {
+            return FindKeyIndexRec(key, 2 * i + 2);
+        }
     }
 
-    public static void main(String[] args) {
-        aBST bst = new aBST(10);
+    public int AddKey(int key) {
+        Integer i = FindKeyIndex(key);
 
+        if (i == null) {
+            return -1;
+        }
 
+        if (Tree[0] == null) {
+            Tree[0] = key;
+            return 0;
+        }
+
+        if (i < 0) {
+            Tree[-i] = key;
+        }
+
+        return i;
     }
 }
